1d58c1c46ac12bee65571349835c1a2a59cc2e1d
GhostGamer6969 <145375630+GhostGamer6969@users.noreply.github.com>
2026-01-09 12:57:42 +0530

    history fix and setup added

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+sudo wget "https://raw.githubusercontent.com/por-cli/por-cli/refs/heads/main/por-cli" -O "/usr/local/bin/por-cli"
+sudo chmod +x "/usr/local/bin/por-cli"
+
+HIS="${XDG_STATE_HOME:-$HOME/.local/state}/por-cli/watch.history"
+mkdir -p "$(dirname "$HIS")"
+touch "$HIS"
+
