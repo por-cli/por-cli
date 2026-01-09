2bb14c2ba95feceb97eed5699c175298c245bcd1
Arambh Mani <145375630+GhostGamer6969@users.noreply.github.com>
2026-01-10 00:13:17 +0530

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-sudo wget "https://raw.githubusercontent.com/por-cli/por-cli/refs/heads/main/por-cli" -O "/usr/local/bin/por-cli"
+sudo wget "https://raw.githubusercontent.com/por-cli/por-cli/refs/heads/main/por-cli" -O "/usr/local/bin"
 sudo chmod +x "/usr/local/bin/por-cli"
 
 HIS="${XDG_STATE_HOME:-$HOME/.local/state}/por-cli/watch.history"
